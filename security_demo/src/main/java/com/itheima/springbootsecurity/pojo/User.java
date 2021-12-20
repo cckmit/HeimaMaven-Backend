1e21962a141c7a7eae945fa72e24f0495ca13763
Finger <finger@spianmo.com>
2021-12-20 21:01:29 +0800

    补充SpringBoot Security例子

--- security_demo/src/main/java/com/itheima/springbootsecurity/pojo/User.java
+++ security_demo/src/main/java/com/itheima/springbootsecurity/pojo/User.java
@@ -1,31 +1,29 @@
-package com.itheima.admin.pojo;
+package com.itheima.springbootsecurity.pojo;
 
-import com.baomidou.mybatisplus.annotation.FieldFill;
-import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.math.BigDecimal;
-import java.util.Date;
-
 /**
- * @author Finger
- */
+ * @ClassName User
+ * @Description TODO
+ * @Author lh
+ * @Date 2021/12/20
+ **/
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @TableName("ss_user")
 public class User {
-    @TableId(value = "user_id")
+    @TableId
     private String userId;
     private String email;
     private String userName;
     private String station;
     private String password;
-    private BigDecimal state;
+    private String state;
     private String managerId;
     private String gender;
     private String telephone;
@@ -38,11 +36,6 @@ public class User {
     private String deptId;
     private String deptName;
     private String createBy;
-    @TableField("create_dept")
     private String create_dept;
-    @TableField(fill = FieldFill.INSERT)
-    private Date createTime;
     private String updateBy;
-    @TableField(fill = FieldFill.UPDATE)
-    private Date updateTime;
 }
